7995355d787b9c0047491b40cef3ef66cab94cab
CALEB KOMEN <komencaleb502@gmail.com>
2024-05-21 21:27:49 +0300

    functions in python

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
+greet_programmer()
 
-def greet(name):
-    pass
+def greet(name= "Sauce_Peter"):
+    print(f"Hello, {name}!")
+greet()    
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+greet_with_default("Lecturer")
 
 def add(num1, num2):
-    pass
+    return(num1 +num2)
 
 def halve(number):
-    pass
+    return(number/2)
